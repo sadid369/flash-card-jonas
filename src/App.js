43bafdd09dd8f3bfc6bc3f49ae1c86d8aa8071c4
Sadid <66120003+sadid369@users.noreply.github.com>
2024-06-12 03:39:59 +0600

    not

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const reactQuestionsAndAnswers = [
+  {
+    id: 101,
+    question: "What is React.js?",
+    answer: "A popular JavaScript library for building user interfaces.",
+  },
+  {
+    id: 102,
+    question: "What is JSX?",
+    answer: "An extension to JavaScript for writing HTML-like code in React.",
+  },
+  {
+    id: 103,
+    question: "What are React components?",
+    answer: "Reusable building blocks for UI logic and rendering.",
+  },
+  {
+    id: 104,
+    question: "What is the virtual DOM?",
+    answer: "In-memory representation of the actual DOM used by React.",
+  },
+  {
+    id: 105,
+    question: "What is state in React?",
+    answer: "An object holding component-specific data.",
+  },
+  {
+    id: 106,
+    question: "What are React hooks?",
+    answer:
+      "Functions for managing state and side effects in functional components.",
+  },
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid-container">
+      <DisplayCard />
     </div>
   );
 }
 
-export default App;
+function DisplayCard() {
+  const [selectedId, setSelectedId] = useState(103);
+  const handleClick = (id) => {
+    setSelectedId(id);
+  };
+
+  return reactQuestionsAndAnswers.map((question) => (
+    <div
+      className={
+        selectedId === reactQuestionsAndAnswers.id
+          ? "grid-item-a"
+          : "grid-item-q"
+      }
+      onClick={() => handleClick(question.id)}
+      key={question.id}
+    >
+      <p>
+        {selectedId === reactQuestionsAndAnswers.id
+          ? question.answer
+          : question.question}
+      </p>
+    </div>
+  ));
+}
